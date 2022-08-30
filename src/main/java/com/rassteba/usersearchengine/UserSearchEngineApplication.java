f66c83a761bf7313c28a2d53b32c194164948548
RasstebaVlad <rassteba.vlad@gmail.com>
2022-08-31 02:33:55 +0300

    First commit

---
+++ src/main/java/com/rassteba/usersearchengine/UserSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.rassteba.usersearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSearchEngineApplication.class, args);
+	}
+
+}
