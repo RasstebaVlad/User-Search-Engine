9c7d7afb2bd62d7b3b7a19d0d018918cd4c53b2a
RasstebaVlad <rassteba.vlad@gmail.com>
2022-09-01 19:06:08 +0300

    Add annotation in UserDto.java

--- src/main/java/com/rassteba/usersearchengine/dto/UserDto.java
+++ src/main/java/com/rassteba/usersearchengine/dto/UserDto.java
@@ -1,13 +1,17 @@
 package com.rassteba.usersearchengine.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
 	String firstName;
